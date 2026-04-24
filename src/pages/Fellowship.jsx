7f87650aec75f57a87e48f2aeb88a8bfcd7d56b1
AbidingAnchor <drewnegron95@gmail.com>
2026-04-24 07:01:55 -0400

    fix: add createPortal import to Fellowship.jsx

--- src/pages/Fellowship.jsx
+++ src/pages/Fellowship.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef, useMemo } from 'react'
+import { createPortal } from 'react-dom'
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { useAuth } from '../context/AuthContext'
